4240d18ec3712e96b1e9d08e1e578f832231ee6d
LT <ltruciosr.dev@gmail.com>
2024-01-24 22:24:15 -0500

    test reading of model blocks

--- tests/test_release1.cpp
+++ tests/test_release1.cpp
@@ -15,7 +15,7 @@ TEST(DATATWIN, RELEASE_1)
     /* Read the parameters */
     std::string data_dir = "/home/ltruciosr-dev/Documents/astay/datatwin/mining_sim/data/";
     std::string db_file = data_dir + "DbDataTwin.sqlite";
-    std::string modelblock_parquet_file = data_dir + "ModelBlocks.parquet";
+    std::string modelblock_parquet_file = data_dir + "ModeloBloques.parquet";
     std::string topography_parquet_file = data_dir + "Topografia.parquet";
     std::string svg_dir = data_dir + "svg/release_1/";
     std::cout << "RELEASE 1 | Store SVG on -> " << svg_dir << std::endl;
@@ -30,17 +30,20 @@ TEST(DATATWIN, RELEASE_1)
     
     /* Read Mining Polygons and Topography */
     datatwin.ReadMiningCuts(false);
+    datatwin.ReadMiningBlocks(false);
     datatwin.ReadTopography(false);
     datatwin.ReadMiningGeoPolygons(false);
-    // datatwin.ReadMiningBlocks(false);
     
     /* Process Information */
-    datatwin.IntersectMiningCutsWithPolylines(true);
-    // -- compute
+    // datatwin.CreateSlicerBlocks(false);
+    datatwin.IntersectMiningCutsWithPolylines(false);
+    
+    /* Compute Intersection */
     // datatwin.ComputeSlices(slice_dist, kShowVerbose);
     // datatwin.ComputeFingerIntersections(kDontVerbose);
-    // -- draw
-    datatwin.Draw(false);
+    
+    /* Draw */
+    datatwin.Draw(true);
 
     // Assert
     ASSERT_EQ(0, 0);
